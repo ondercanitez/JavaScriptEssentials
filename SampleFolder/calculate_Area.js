7007044e9b02cdda72804ca352840ca2428738d7
ondercanitez <onder.canitez@gmail.com>
2025-08-29 21:20:48 +0300

    area

--- SampleFolder/calculate_Area.js
+++ SampleFolder/calculate_Area.js
@@ -1,10 +1,8 @@
-let length;
-let width;
-
 function calculateArea() {
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
+  let length = parseFloat(document.getElementById('length').value);
+  let width = parseFloat(document.getElementById('width').value);
 
-let area = length * width;
+  
+  let area = length * width;
+  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
